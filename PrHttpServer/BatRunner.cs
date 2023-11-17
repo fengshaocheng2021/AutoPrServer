254bb1498adad1c7bfa0fa92aa30526b30ea8ba0
shaocheng <fengshaocheng@pointone.tech>
2023-11-17 17:18:46 +0800

    git pr server

---
+++ PrHttpServer/BatRunner.cs
@@ -0,0 +1,29 @@
+﻿using System.Diagnostics;
+
+namespace SimpleHttpServer;
+
+public class BatRunner
+{
+    public static void RunBat(string prNum)
+    {
+        Process proc = null;
+        try
+        {
+            // string targetDir = string.Format(@"D:\BizMap\"); //这是bat存放的目录
+            // string targetDir = string.Format(@"D:\"); //这是bat存放的目录
+            string targetDir1 = AppDomain.CurrentDomain.BaseDirectory; //或者这样写，获取程序目录
+            proc = new Process();
+            proc.StartInfo.WorkingDirectory = targetDir1;
+            proc.StartInfo.FileName = "GitAutoPr.bat"; //bat文件名称
+            proc.StartInfo.Arguments = string.Format(prNum); //this is argument
+            //proc.StartInfo.CreateNoWindow = true;
+            proc.StartInfo.WindowStyle = ProcessWindowStyle.Hidden;//这里设置DOS窗口不显示，经实践可行
+            proc.Start();
+            proc.WaitForExit();
+        }
+        catch (Exception ex)
+        {
+            Console.WriteLine("Exception Occurred :{0},{1}", ex.Message, ex.StackTrace.ToString());
+        }
+    }
+}
---
+++ PrHttpServer/bin/Debug/net8.0/GitAutoPr.bat
@@ -0,0 +1,6 @@
+
+cd D:\BUD_GAME_GLOBAL_Framework
+
+set pr1=%1
+echo %pr1%
+gh pr review %pr1% -a
